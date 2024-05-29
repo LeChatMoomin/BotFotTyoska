16654c078f9524b02e68599f1ae8588b569b6ad8
Alexey K <rankedme@gmail.com>
2024-05-29 23:30:47 +0300

    Refactor SQLManager

--- MyBot/Bot/SQLManager.cs
+++ MyBot/Bot/SQLManager.cs
@@ -29,7 +29,7 @@ namespace MyBot.DataBase
 					using (var reader = new MySqlCommand(saveCommand, connection).ExecuteReader()) {
 						if (reader.Read()) {
 							for (int i = 0; i < data.Characters.Count; i++) {
-								SaveCharacter(connection, data.Characters[i]);
+								Save(data.Characters[i]);
 							}
 						}
 					}
@@ -45,18 +45,7 @@ namespace MyBot.DataBase
 			try {
 				using (var connection = new MySqlConnection(ConnectionString)) {
 					connection.Open();
-					SaveCharacter(connection, data);
-					connection.Close();
-				}
-			} catch (Exception e) {
-				Console.WriteLine($"Не удалось сохраниться, вот ошибка: {e.Message}");
-			}
-		}
-
-		private void SaveCharacter(MySqlConnection connection, CharacterData data)
-		{
-			try {
-				var updateCharCommand = $"UPDATE '{DbName}'.'Characters' SET " +
+					var updateCharCommand = $"UPDATE '{DbName}'.'Characters' SET " +
 					$"'Name' = {data.Name}, " +
 					$"'Level' = {data.Level}, " +
 					$"'Phy' = {data.Phy}, " +
@@ -69,10 +58,12 @@ namespace MyBot.DataBase
 					$"'State' = {(int)data.State}, " +
 					$"'Gold' = {data.Gold} " +
 					$"WHERE Character_id = {data.Id}";
-				var reader = new MySqlCommand(updateCharCommand, connection).ExecuteReader();
-				reader.Read();
+					var reader = new MySqlCommand(updateCharCommand, connection).ExecuteReader();
+					reader.Read();
+					connection.Close();
+				}
 			} catch (Exception e) {
-				Console.WriteLine(e.Message);
+				Console.WriteLine($"Не удалось сохраниться, вот ошибка: {e.Message}");
 			}
 		}
 
@@ -137,7 +128,7 @@ namespace MyBot.DataBase
 					using (var reader = new MySqlCommand(loadUserCommand, connection).ExecuteReader()) {
 						if (reader.Read()) {
 							data.State = (PlayerState)reader.GetInt32("State");
-							data.Characters = GetCharsForUser(connection, id);
+							data.Characters = GetCharsForUser(id);
 						} else {
 							SaveNewPlayer(data);
 						}
@@ -153,46 +144,35 @@ namespace MyBot.DataBase
 
 		public List<CharacterData> GetCharsForUser(long ownerId)
 		{
+			var result = new List<CharacterData>();
 			try {
 				using (var connection = new MySqlConnection(ConnectionString)) {
 					connection.Open();
-					var result = GetCharsForUser(connection,ownerId);
+					var loadCharactersCommand = $"SELECT * FROM Characters WHERE Owner = {ownerId}";
+					using (var reader = new MySqlCommand(loadCharactersCommand, connection).ExecuteReader()) {
+						while (reader.Read()) {
+							var data = new CharacterData {
+								Id = reader.GetInt32("Character_id"),
+								OwnerId = reader.GetInt64("Owner"),
+								Name = reader.GetString("Name"),
+								Level = reader.GetInt32("Level"),
+								Phy = reader.GetInt32("Phy"),
+								Str = reader.GetInt32("Str"),
+								Agi = reader.GetInt32("Agi"),
+								Int = reader.GetInt32("Int"),
+								Gold = reader.GetInt32("Gold"),
+								State = (CharacterState)reader.GetInt32("State"),
+								Weapon = GetItem(reader.GetInt32("Weapons_Weapon_id"), ItemSlot.Weapon),
+								Armor = GetItem(reader.GetInt32("Armors_Armor_id"), ItemSlot.Armor),
+								Potion = GetItem(reader.GetInt32("Potions_Potion_id"), ItemSlot.Potion),
+							};
+							result.Add(data);
+						}
+					}
 					connection.Close();
-					return result;
 				}
 			} catch (Exception e) {
 				Console.WriteLine($"Не удалось загрузать персонажей, вот ошибка: {e.Message}");
-				throw;
-			}
-		}
-
-		private List<CharacterData> GetCharsForUser(MySqlConnection connection, long ownerId)
-		{
-			var result = new List<CharacterData>();
-			try {
-				var loadCharactersCommand = $"SELECT * FROM Characters WHERE Owner = {ownerId}";
-				using (var reader = new MySqlCommand(loadCharactersCommand, connection).ExecuteReader()) {
-					while (reader.Read()) {
-						var data = new CharacterData {
-							Id = reader.GetInt32("Character_id"),
-							OwnerId = reader.GetInt64("Owner"),
-							Name = reader.GetString("Name"),
-							Level = reader.GetInt32("Level"),
-							Phy = reader.GetInt32("Phy"),
-							Str = reader.GetInt32("Str"),
-							Agi = reader.GetInt32("Agi"),
-							Int = reader.GetInt32("Int"),
-							Gold = reader.GetInt32("Gold"),
-							State = (CharacterState)reader.GetInt32("State"),
-							Weapon = GetItem(connection, reader.GetInt32("Weapons_Weapon_id"), Game.ItemSlot.Weapon),
-							Armor = GetItem(connection, reader.GetInt32("Armors_Armor_id"), Game.ItemSlot.Armor),
-							Potion = GetItem(connection, reader.GetInt32("Potions_Potion_id"), Game.ItemSlot.Potion),
-						};
-						result.Add(data);
-					}
-				}
-			} catch (Exception e) {
-				Console.WriteLine(e.Message);
 			}
 			return result;
 		}
@@ -202,78 +182,56 @@ namespace MyBot.DataBase
 			try {
 				using (var connection = new MySqlConnection(ConnectionString)) {
 					connection.Open();
-					var result = GetCharacter(connection, id);
-					connection.Close();
-					return result;
-				}
-			} catch (Exception e) {
-				Console.WriteLine($"Не удалось загрузить перса, вот ошибка: {e.Message}");
-				throw;
-			}
-		}
-
-		private CharacterData GetCharacter(MySqlConnection connection, int id)
-		{
-			try {
-				var loadCharactersCommand = $"SELECT * FROM Characters WHERE Character_id = {id}";
-				using (var reader = new MySqlCommand(loadCharactersCommand, connection).ExecuteReader()) {
-					if (reader.Read()) {
-						var data = new CharacterData {
-							Id = reader.GetInt32("Character_id"),
-							OwnerId = reader.GetInt64("Owner"),
-							Name = reader.GetString("Name"),
-							Level = reader.GetInt32("Level"),
-							Phy = reader.GetInt32("Phy"),
-							Str = reader.GetInt32("Str"),
-							Agi = reader.GetInt32("Agi"),
-							Int = reader.GetInt32("Int"),
-							Gold = reader.GetInt32("Gold"),
-							State = (CharacterState)reader.GetInt32("State"),
-							Weapon = GetItem(connection, reader.GetInt32("Weapons_Weapon_id"), Game.ItemSlot.Weapon),
-							Armor = GetItem(connection, reader.GetInt32("Armors_Armor_id"), Game.ItemSlot.Armor),
-							Potion = GetItem(connection, reader.GetInt32("Potions_Potion_id"), Game.ItemSlot.Potion),
-						};
-						return data;
+					var loadCharactersCommand = $"SELECT * FROM Characters WHERE Character_id = {id}";
+					using (var reader = new MySqlCommand(loadCharactersCommand, connection).ExecuteReader()) {
+						if (reader.Read()) {
+							var data = new CharacterData {
+								Id = reader.GetInt32("Character_id"),
+								OwnerId = reader.GetInt64("Owner"),
+								Name = reader.GetString("Name"),
+								Level = reader.GetInt32("Level"),
+								Phy = reader.GetInt32("Phy"),
+								Str = reader.GetInt32("Str"),
+								Agi = reader.GetInt32("Agi"),
+								Int = reader.GetInt32("Int"),
+								Gold = reader.GetInt32("Gold"),
+								State = (CharacterState)reader.GetInt32("State"),
+								Weapon = GetItem(reader.GetInt32("Weapons_Weapon_id"), ItemSlot.Weapon),
+								Armor = GetItem(reader.GetInt32("Armors_Armor_id"), ItemSlot.Armor),
+								Potion = GetItem(reader.GetInt32("Potions_Potion_id"), ItemSlot.Potion),
+							};
+							return data;
+						}
 					}
+					connection.Close();
 				}
-				return null;
 			} catch (Exception e) {
 				Console.WriteLine($"Не удалось загрузить перса, вот ошибка: {e.Message}");
-				throw;
 			}
+			return null;
 		}
 
 		public ItemData GetItem(int id, ItemSlot slot)
 		{
+			var result = new ItemData { Id = id };
 			try {
 				using (var connection = new MySqlConnection(ConnectionString)) {
 					connection.Open();
-					var result = GetItem(connection, id, slot);
+					var loadItemCommand = $"SELECT * FROM {slot}s WHERE {slot}_id = {id}";
+					using (var reader = new MySqlCommand(loadItemCommand, connection).ExecuteReader()) {
+						if (reader.Read()) {
+							result.Name = reader.GetString("Name");
+							result.Value = reader.GetInt32("Value");
+							result.Cost = reader.GetInt32("Cost");
+						}
+					}
 					connection.Close();
 					return result;
 				}
 			} catch (Exception e) {
-				Console.WriteLine($"Не удалось сохраниться, вот ошибка: {e.Message}");
-				throw;
+				Console.WriteLine($"Не удалось загрузить предмет, вот ошибка: {e.Message}");
 			}
-		}
-
-		private ItemData GetItem(MySqlConnection connection, int id, ItemSlot slot)
-		{
-			var result = new ItemData { Id = id };
-			try {
-				var loadItemCommand = $"SELECT * FROM {slot}s WHERE {slot}_id = {id}";
-				using (var reader = new MySqlCommand(loadItemCommand, connection).ExecuteReader()) {
-					if (reader.Read()) {
-						result.Name = reader.GetString("Name");
-						result.Value = reader.GetInt32("Value");
-						result.Cost = reader.GetInt32("Cost");
-					}
-				}
-			} catch (Exception e) {
-				Console.WriteLine(e.Message);
-			}
-			return result;
+			return null;
 		}
 		#endregion
 
